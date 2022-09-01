d6bcf4b7e81a621ddb73a9724884df8352d1da4f
jawaTeapot <jawa_teapot@bk.ru>
2022-09-01 19:28:51 +0300

    modals

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ $(document).ready(function(){
     $('#registration').slick({
         dots: true,
     });
+
     const $services = $('#services');
     const $slideCount = $('#slide-count');
 
@@ -15,4 +16,20 @@ $(document).ready(function(){
         prevArrow: $('#prev-arrow'),
         nextArrow: $('#next-arrow')
     });
+
+    const $gifts = $('#gifts');
+    const $countSlide = $('#count-slide');
+
+    $gifts.on('init reInit afterChange', function(event, slick, currentSlide){
+        const i = (currentSlide ? currentSlide : 0) + 1;
+        $countSlide.text(i + '/' + slick.countSlide);
+    });
+
+    $gifts.slick({
+        infinite: false,
+        prevArrow: $('#prev-arrow'),
+        nextArrow: $('#next-arrow')
+    });
+
 });
+gifts
